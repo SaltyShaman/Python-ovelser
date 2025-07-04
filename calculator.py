e61bdc3640f3899e891ca2fa2965c5873e585a8c
SaltyShaman <kristoffer6400@gmail.com>
2025-07-04 13:56:28 +0200

    Splitting the file into seperate files

---
+++ calculator.py
@@ -0,0 +1,20 @@
+# calculator.py
+
+def calculator(a, operator, b):
+    if not isinstance(a, (int, float)):
+        raise TypeError("Første parameter skal være et tal")
+    if not isinstance(b, (int, float)):
+        raise TypeError("Andet parameter skal være et tal")
+    if operator not in ['+', '-', '*', '/']:
+        raise ValueError("Operator skal være en af '+', '-', '*', '/'")
+
+    if operator == '+':
+        return a + b
+    elif operator == '-':
+        return a - b
+    elif operator == '*':
+        return a * b
+    elif operator == '/':
+        if b == 0:
+            raise ZeroDivisionError("Division med nul er ikke tilladt")
+        return a / b
